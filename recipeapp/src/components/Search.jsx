c135dab46ee97340a8205e0af9f96fad2e8c9384
Monark <efehiidubor@gmail.com>
2024-11-28 12:23:20 +0100

    useEffect hook implemented
    useEffect Hook implemented to fetch recipes from API when query changes.

--- recipeapp/src/components/Search.jsx
+++ recipeapp/src/components/Search.jsx
@@ -1,7 +1,19 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
+
+const URL = "https://api.spoonacular.com/recipes/complexSearch";
+const API_KEY = "cdf248bd409040c9a806e53ca61ad83b";
 
 export default function Search() {
-  const [query, setQuery] = useState("");
+  const [query, setQuery] = useState("pizza");
+  //syntax of the useEfect hook
+  useEffect(() => {
+    async function fetchRecipe() {
+      const res = await fetch(`${URL}?query=${query}&apiKey=${API_KEY}`);
+      const data = await res.json();
+      console.log(data.results);
+    }
+    fetchRecipe();
+  }, [query]);
   return (
     <div>
       <input
