341a92f52cb20cdf4eceeb133b7b4cc5342fff27
Monark <efehiidubor@gmail.com>
2024-11-29 16:05:34 +0100

    implemented UI for recipelist using css

--- recipeapp/src/App.jsx
+++ recipeapp/src/App.jsx
@@ -1,17 +1,19 @@
 import { useState } from "react";
 import Search from "./components/Search";
+import RecipeList from "./components/RecipeList";
 
 function App() {
   const [recipeData, setRecipeData] = useState([]);
   return (
     <div className="App">
       <Search recipeData={recipeData} setRecipeData={setRecipeData} />
-      {recipeData.map((recipe) => (
+      <RecipeList recipeData={recipeData} />
+      {/* {recipeData.map((recipe) => (
         <div key={recipe.id}>
           <h1>{recipe.title}</h1>
           <img src={recipe.image} />
         </div>
-      ))}
+      ))} */}
     </div>
   );
 }
--- recipeapp/src/components/RecipeList.jsx
+++ recipeapp/src/components/RecipeList.jsx
@@ -1,3 +1,14 @@
-export default function RecipeList() {
-  return <div></div>;
+import styles from "./recipelist.module.css";
+
+export default function RecipeList({ recipeData }) {
+  return (
+    <div className={styles.listPane}>
+      {recipeData.map((recipe) => (
+        <div className={styles.recipeItem} key={recipe.id}>
+          <img src={recipe.image} />
+          <h3 className={styles.recipeTitle}>{recipe.title}</h3>
+        </div>
+      ))}
+    </div>
+  );
 }
